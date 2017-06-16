87ed3578597816a2cd1bd2d2934cfc838bcb2435
kevin julio <kevinjulio93@outlook.com>
2017-06-15 23:43:20 -0500

    agregan db

--- index.py
+++ index.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request
+from flask.ext.mysqldb import MySQL
+
 app = Flask(__name__)
 
-@app.route("/send", methods=['GET', 'POST'])
-def send():
+app.config['MYSQL_HOST']= '172.0.0.1'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= 'Ormuco'
+mysql.init_app(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def add():
+    
     if request.method == 'POST':
-        num1 = int(request.form['numero1'])
-        num2 = int(request.form['numero2'])
+        
+        name = request.form['Name']
+        favcolor = request.form['Favcolor']
+        cot = request.form['CoT']
 
-        a = num1 + num2
+        
 
-        resultado = str(a)
+        
 
         return render_template('age.html', age=resultado)
     return render_template('index.html')
