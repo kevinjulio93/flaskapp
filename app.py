09d21871bc6166ed050e0c284573de3b3f8bac83
kevinjulio93 <kevinjulio93@outlook.com>
2017-06-19 02:53:26 -0500

    app already finished

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request
-import pymysql.cursors
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 
 app = Flask(__name__)
 
-db = SQLAlchemy
+db = SQLAlchemy().session
 from app_migrations import *
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='252601993',
-                             db='Ormuco',
-                             charset='utf8mb4')
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='252601993',
+#                              db='Ormuco',
+#                              charset='utf8mb4')
 
 
 @app.route("/", methods=['GET', 'POST'])
 def add():
-
+    mensaje =""
     if request.method == 'POST':
         _name = request.form['Name']
         _favcolor = request.form['Favcolor']
@@ -24,12 +25,20 @@ def add():
 
         data = User(_name, _favcolor, _cot)
 
-        db.session.add(data)
-        db.session.commit()
+        try:
+            db.add(data)
+            db.commit()
+            mensaje = 'Saved Sucesfully'
+        except exc.IntegrityError:
+            db.rollback()
+            mensaje = "The name already exist, please set an other"
+        finally:
+            db.close()
 
     q = User.query.all()
-    return render_template('index.html', querys = q)
+    return render_template('index.html', querys=q, msn = mensaje)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
