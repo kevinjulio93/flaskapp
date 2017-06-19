fac120e725a0628008f752829ccb9e092f330cb5
kevinjulio93 <kevinjulio93@outlook.com>
2017-06-19 00:29:28 -0500

    save and show data

--- app.py
+++ app.py
@@ -5,37 +5,30 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 db = SQLAlchemy
-
 from app_migrations import *
 
 connection = pymysql.connect(host='localhost',
-                           user='root',
-                          password='252601993',
-                         db='Ormuco',
-                        charset='utf8mb4')
+                             user='root',
+                             password='252601993',
+                             db='Ormuco',
+                             charset='utf8mb4')
 
-# connection = pymysql.connect(host='sql10.freemysqlhosting.net',
-#                              user='sql10180646',
-#                              password='CMVHyy9b3U',
-#                              db='sql10180646',
-#                              charset='utf8mb4')
-
-
-# cursor=pymysql.cursors.DictCursor)
 
 @app.route("/", methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
 
+    if request.method == 'POST':
         _name = request.form['Name']
         _favcolor = request.form['Favcolor']
         _cot = request.form['CoT']
 
         data = User(_name, _favcolor, _cot)
+
         db.session.add(data)
         db.session.commit()
 
-    return render_template('index.html')
+    q = User.query.all()
+    return render_template('index.html', querys = q)
 
 
 if __name__ == '__main__':
